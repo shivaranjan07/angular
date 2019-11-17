84767d2d2be136c335b66964bc99cc9a1a70b495
= <shivaranjan421@gmail.com>
2019-11-17 23:39:57 +0530

    SHI-0000|shivaranjan07|components creation related code been added eg. server and servers folders

--- hello-app/src/app/app.module.ts
+++ hello-app/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServersComponent,
+    ServerComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ hello-app/src/app/server/server.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({ // decorator enhances the classes
+ selector: 'app-server',
+ templateUrl: './server.component.html'
+})
+export class ServerComponent {
+}
